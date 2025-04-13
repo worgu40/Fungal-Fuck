956d67aae62aa8064ac055199077acb8c1caba97
FloatingMouse <59693625+FloatingMouse@users.noreply.github.com>
2025-04-12 22:21:49 -0400

    Added Player Rotation
    ADD: Player rotation.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,6 +15,7 @@ public class Player : MonoBehaviour
     }
     private CharacterController controller;
     public static Player instance;
+    [SerializeField] GameObject playerModel;
     void Start()
     {
         instance = this;
@@ -29,6 +30,14 @@ public class Player : MonoBehaviour
             Vector3 movementCreation = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             movementCreation = movementCreation.normalized * Mathf.Clamp01(movementCreation.magnitude); //clamps magnitude of the vector between 0 and 1
             controller.Move(movementCreation * Speed * Time.deltaTime);
+            if(movementCreation.x == 0 && movementCreation.z != 0)
+            {
+                playerModel.transform.localRotation = Quaternion.Euler(0,movementCreation.z < 0 ? -90 : 90,0);
+            }
+            else if(movementCreation.x != 0)
+            {
+                playerModel.transform.localRotation = Quaternion.Euler(0, (Mathf.Atan(-movementCreation.z / movementCreation.x) + (movementCreation.x<0? 0 : Mathf.PI)) * 180 / Mathf.PI, 0);
+            }
         }
         // Health
         healthBar.fillAmount = health / 100f;
