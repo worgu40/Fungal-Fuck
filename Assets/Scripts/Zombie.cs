7bf92fcf7dca6430c88da60b91ff0dbed00f3781
worgu40 <marshbutstupid@gmail.com>
2025-04-12 21:52:22 -0400

    Zombie AI
    ADD: Patrol, Chase, and Attack phases for the Zombie. Also added Player and Ground layer
    UPDATE: Changed where damage is dealt.

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -8,6 +8,13 @@ public class Zombie : MonoBehaviour
     private NavMeshAgent navAI;
     public float distanceToPlayer;
     private bool canAttack = true;
+    private Vector3 walkPoint;
+    private bool walkPointSet;
+    public float walkPointRange;
+    public LayerMask groundLayer, playerLayer;
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+    private bool touchingPlayer = false;
     void Start()
     {
         navAI = GetComponent<NavMeshAgent>();
@@ -16,16 +23,71 @@ public class Zombie : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, playerLayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
         navAI.SetDestination(Player.instance.transform.position);
         distanceToPlayer = Vector3.Distance(Player.instance.transform.position, transform.position);
+        if (!playerInSightRange && !playerInAttackRange) {
+            Patroling();
+        }
+        if (playerInSightRange && !playerInAttackRange) {
+            ChasePlayer();
+        }
+        if (playerInAttackRange && playerInSightRange) {
+            AttackPlayer();
+        }
         if (Player.instance.health <= 0) {
             canAttack = false;
+        }
+    }
+    private void Patroling() {
+        if (!walkPointSet) {
+            SearchWalkPoint();
+        }
+        if (walkPointSet) {
+            navAI.SetDestination(walkPoint);
+        }
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        // Point reached
+        if (distanceToWalkPoint.magnitude < 1f) {
+            walkPointSet = false;
+        }
+    }
+    private void SearchWalkPoint() {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, groundLayer)) {
+            walkPointSet = true;
+        }
+    }
+    private void AttackPlayer() {
+        if (!touchingPlayer) {
+            navAI.SetDestination(Player.instance.transform.position);
+        }
+        if (touchingPlayer) {
+            navAI.SetDestination(transform.position);
+        }
+
+        if (!touchingPlayer) {
+            Vector3 direction = Player.instance.transform.position - transform.position;
+            direction.y = 0;
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 5f);
         }
         if (distanceToPlayer <= 1.1f && canAttack) {
             StartCoroutine(DealDamage(20f));
         }
     }
-
+    private void ChasePlayer() {
+        navAI.SetDestination(Player.instance.transform.position);
+    }
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
     private IEnumerator DealDamage(float damage) {
         canAttack = false;
         Player.instance.health -= damage;
